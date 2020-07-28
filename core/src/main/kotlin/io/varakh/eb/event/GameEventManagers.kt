69eec6835d788a145d0a5f980c912f38ab6dce55
Janik Lüthi <nerachus@gmail.com>
2020-07-28 02:14:45 +0200

    Inlining is complicated.

--- core/src/main/kotlin/io/varakh/eb/event/GameEventManagers.kt
+++ core/src/main/kotlin/io/varakh/eb/event/GameEventManagers.kt
@@ -3,11 +3,10 @@ package io.varakh.eb.event
 import kotlin.reflect.KClass
 
 object GameEventManagers {
-    val managers = mutableMapOf<KClass<*>, GameEventManager<*>>()
+    private val managers = mutableMapOf<KClass<*>, GameEventManager<*>>()
 
     @Suppress("UNCHECKED_CAST")
-    inline operator fun <reified T : GameEvent> get(type: KClass<T>): GameEventManager<T> =
-            managers[type] as GameEventManager<T>? ?: GameEventManager(type).also {
-                managers[type] = it
-            }
+    operator fun <T : GameEvent> get(type: KClass<T>) =
+            managers[type] as GameEventManager<T>?
+                    ?: GameEventManager(type).also { managers[type] = it }
 }
